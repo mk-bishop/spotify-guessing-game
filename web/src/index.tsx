744b49571aa1fc9a5ea3870a67a5a99d93c2485b
MK Dean <marykathryn.dean@gmail.com>
2026-06-11 15:28:45 -0400

    initial commit

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './redux/store';
+// import { store } from './redux/store';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
@@ -13,9 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      {/* <Provider store={store}> */}
+      <App />
+      {/* </Provider> */}
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
---
+++ web/src/redux/store.ts
@@ -0,0 +1,12 @@
+// import { applyMiddleware, createStore } from 'redux';
+// import thunk from 'redux-thunk';
+
+// import reducer from '../reducers';
+
+// export default function configureStore() {
+//   const middleware = applyMiddleware(thunk);
+//   return createStore(reducer, middleware);
+// }
+
+// export const store = configureStore();
+export default {}
